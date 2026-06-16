7373b080f08f9f5e319b3e662c17bb5be0eb3ec8
Graf-Lab <grafbrum@gmail.com>
2026-06-16 13:46:10 +0200

    Moved NavBar componet

--- src/components/sections/NavBar.jsx
+++ src/components/sections/NavBar.jsx
@@ -1,50 +1,39 @@
 import { useEffect } from "react";
 
-export const NavBar = (menuOpen, setMenuOpen) => {
+export const NavBar = ({ menuOpen, setMenuOpen }) => {
   useEffect(() => {
     document.body.style.overflow = menuOpen ? "hidden" : "";
-  }, [menuOpen]);
+  }, [menuOpen, setMenuOpen]);
   return (
-    <nav className="fixed top-0 w-full z-40 bg-[rgba(10, 10, 10, .8)] backdrop:blur-lg border-b border-white/10 shadow-lg">
+    <nav className="fixed top-0 w-full z-40 bg-[rgba(10, 10, 10, .8)] backdrop-blur-lg border-b border-white/10 shadow-lg">
       <div className="max-w-5xl mx-auto px-4">
         <div className="flex justify-between items-center h-16">
           <a href="#home" className="font-mono text-xl font-bold text-white">
             {" "}
             Graf-Lab<span className="text-blue-500">.github</span>{" "}
           </a>
-          <div
+          <button
             className="w-7 h-5 relative cursor-pointer z-40 md:hidden"
             onClick={() => setMenuOpen((prev) => !prev)}
+            aria-label="Open Menu"
           >
             &#9776;
-          </div>
+          </button>
 
           <div className="hidden md:flex items-center space-x-8">
-            <a
-              href="#home"
-              className="tex-gray-300 text-white transitiontransition-colors"
-            >
+            <a href="#home" className="text-gray-300 transition-colors">
               {" "}
               Home{" "}
             </a>
-            <a
-              href="#about"
-              className="tex-gray-300 text-white transitiontransition-colors"
-            >
+            <a href="#about" className="text-gray-300 transition-colors">
               {" "}
               About{" "}
             </a>
-            <a
-              href="#projects"
-              className="tex-gray-300 text-white transitiontransition-colors"
-            >
+            <a href="#projects" className="text-gray-300 transition-colors">
               {" "}
               Projects{" "}
             </a>
-            <a
-              href="#contact"
-              className="tex-gray-300 text-white transitiontransition-colors"
-            >
+            <a href="#contact" className="text-gray-300 transition-colors">
               {" "}
               Contact{" "}
             </a>
