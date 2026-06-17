5560d497b7bc40e3a840f4efabb6ff41d57cd442
graf-lab <grafbrum@gmail.com>
2026-06-17 09:31:29 +0200

    Update Home.jsx

--- src/components/sections/Home.jsx
+++ src/components/sections/Home.jsx
@@ -25,13 +25,13 @@ export const Home = () => {
         <div className="flex justify-center space-x-4">
           <a
             href="projects"
-            className="bg-blue-500 text-white py-3 rounded font-medium transition relative overflow-hidden hover:-traslate-y-0.5 hover:shadow-[0_0_15px_rgba(59,130,246,0.4)]"
+            className="bg-blue-500 text-white py-3 px-6 rounded font-medium transition relative overflow-hidden hover:-traslate-y-0.5 hover:shadow-[0_0_15px_rgba(59,130,246,0.4)]"
           >
             View Projects
           </a>
           <a
             href="contact"
-            className="bg-blue-500 text-white py-3 rounded font-medium transition relative overflow-hidden hover:-traslate-y-0.5 hover:shadow-[0_0_15px_rgba(59,130,246,0.4)]"
+            className="border border-blue-500 text-blue-500 py-3 px-6 rounded font-medium transition relative overflow-hidden hover:-traslate-y-0.5 hover:shadow-[0_0_15px_rgba(59,130,246,0.4)]"
           >
             Contact Me
           </a>
